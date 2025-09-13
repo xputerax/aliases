eda6ed646e549c70674f455957b13c6078549a22
Aiman Daniel <aiman2301@gmail.com>
2025-09-14 05:58:30 +0800

    fix python.sh

--- scripts/python.sh
+++ scripts/python.sh
@@ -2,7 +2,8 @@
 activate() {
     if [ ! -f "./.venv/bin/activate" ]; then
         echo "./.venv/bin/activate not found, exiting"
+	else
+		. .venv/bin/activate
     fi
 
-    . .venv/bin/activate
 }
